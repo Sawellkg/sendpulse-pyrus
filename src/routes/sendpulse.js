5e45313b78f2406de1bbb2d2d93c67c2f6a2d06b
Sawellkg <s.kg@bk.ru>
2026-03-15 18:59:12 +0600

    update 15-03-2026_senderName5

--- src/routes/sendpulse.js
+++ src/routes/sendpulse.js
@@ -409,18 +409,12 @@ async function handleOutgoing(event) {
     ? `${sentBy.firstname || ''} ${sentBy.lastname || ''}`.trim() || sentBy.email || 'Оператор SP'
     : 'Бот';
 
-  // await pyrusApi.sendIncomingMessage({
-  //   accountId: account.sp_bot_id,
-  //   channelId: contact.id,
-  //   senderName: contact.username || contact.name || 'Неизвестный',
-  //   messageText: `[→ ${authorLabel}]: ${messageText}`,
-  //   messageId: mid || undefined,
-  // });
   await pyrusApi.sendIncomingMessage({
     accountId: account.sp_bot_id,
     channelId: contact.id,
-    senderName: authorLabel,
-    messageText,
+    senderName: 'Bot',
+    //senderName: contact.username || contact.name || 'Неизвестный',
+    messageText: `[→ ${authorLabel}]: ${messageText}`,
     messageId: mid || undefined,
   });
 }
